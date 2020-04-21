e646834e467992f161c6c82100595a6771931c28
Adriane Ribeiro <adrianeribeiro94@gmail.com>
2020-04-21 16:59:36 -0300

    aula 3

--- app/javascript/screens/search/index.js
+++ app/javascript/screens/search/index.js
@@ -1,10 +1,16 @@
 import React, { Fragment} from 'react';
 import NavbarFooter from '../../components/common/navbar_footer'
+import SectionWrapper from '../../components/common/section_wrapper'
+import Search from '../../components/search';
+import { Heading } from 'react-bulma-components';
 
 const SearchScreen = () => {
   return(
     <Fragment>
-      <div>Search Screen</div>
+      <SectionWrapper>
+        <Heading className='has-text-centered has-text-white'>Buscar</Heading>
+        <Search/>
+      </SectionWrapper>
       <NavbarFooter/>
     </Fragment>
   )
